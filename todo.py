e1750c83511bb825df7a8559d4e3cf62762feff6
eva montgomery <eva.m.montgomery@gmail.com>
2019-11-13 14:49:26 -0500

    building a todo app

---
+++ todo.py
@@ -0,0 +1,11 @@
+# to-do's: 
+# -> todo's are a collection of individual variables in list as a string
+
+# choose one -> adding a todo
+# Enter a todo -> prompt user for the new todo then .append() to list of todo's
+
+# Use a for loop to print each todo in our list of todo's
+
+# how can I delete item from a list?
+#  --> del keyword and the index
+# ex. del pets[1] -> deletes second item in pet list
